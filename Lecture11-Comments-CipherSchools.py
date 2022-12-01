66a9677776a1552200db87c9f5e12d1579e5dcc1
Tanish730 <114794096+Tanish730@users.noreply.github.com>
2022-12-01 22:11:18 +0530

    Add files via upload

---
+++ Lecture11-Comments-CipherSchools.py
@@ -0,0 +1,7 @@
+# this is a comment 
+print("Hello World!")
+print('Abhishek Singh') #its my name 
+print('12214677') #its my registration number
+print('KOC09-RKOC0963') # its my section & roll number
+'''I am learning python coding langaunge from CipherSchools
+'''
